261acdf2d3c2788b30f1e68d1765987feada4c33
Pavel <asinskijp188@gmail.com>
2025-07-22 09:34:04 +0300

    добавлен токен песочницы в set_configs.py, исправлены ошибки запроса свечей в классе InfoRequester

--- info_requester.py
+++ info_requester.py
@@ -1,5 +1,9 @@
 from datetime import timedelta
+from http.client import responses
 from typing import Generator
+
+from tinkoff.invest.services import Services
+
 from logger import Logger
 
 from tinkoff.invest import Client, CandleInterval, HistoricCandle
@@ -12,9 +16,9 @@ from set_config import TOKEN
 class InfoRequester:
     """ Класс для запроса информации от инвестиций """
 
-    def __init__(self, is_sandbox: bool = False):
+    def __init__(self, client: Services, is_sandbox: bool = False):
         self.sandbox_mode = is_sandbox
-        self.token = TOKEN
+        self.client: Services = client
         self.target: INVEST_GRPC_API | INVEST_GRPC_API_SANDBOX = INVEST_GRPC_API  # режим обычный\песочница
         self.logger = Logger()
 
@@ -23,7 +27,7 @@ class InfoRequester:
             figi: str,
             days_back: int = 1,
             timeframe: CandleInterval = CandleInterval.CANDLE_INTERVAL_HOUR
-    ) -> Generator[HistoricCandle, None, None]:
+    ) -> Generator[HistoricCandle, None, None] | None:
         """
         Получение свечей актива за определенный период
         :param figi: идентификатор figi актива
@@ -31,15 +35,15 @@ class InfoRequester:
         :param timeframe: период построение свечей (по умолчанию 1 час)
         :return: генератор с данными о свечах по заданным параметрам
         """
-        with Client(self.token, target=self.target) as client:
-            try:
-                self.logger.info(message=f"figi [{figi}] - старт получения свечей [days_back:{days_back}, timeframe:{timeframe}]")
-                response = client.get_all_candles(
-                    from_=now() - timedelta(days_back),
-                    to=now(),
-                    figi=figi,
-                    interval=timeframe
-                )
-            except Exception as ex:
-                self.logger.error(message=f"figi [{figi}] - получение свечей завершено с ошибкой: {ex}", module=__name__)
-            return response
+        response = None
+        try:
+            self.logger.info(message=f"figi [{figi}] - старт получения свечей [days_back:{days_back}, timeframe:{timeframe}]")
+            response = self.client.get_all_candles(
+                from_=now() - timedelta(days_back),
+                to=now(),
+                figi=figi,
+                interval=timeframe
+            )
+        except Exception as ex:
+            self.logger.error(message=f"figi [{figi}] - получение свечей завершено с ошибкой: {ex}", module=__name__)
+        return response
--- set_config.py
+++ set_config.py
@@ -5,3 +5,4 @@ _parser.read(".configs.ini")
 
 
 TOKEN: str = _parser["TOKENS"]["token"]
+SANDBOX_TOKEN: str = _parser["TOKENS"]["sandbox"]
