724167442de82e8749ee2b82917768a243baa57b
Pavel <asinskijp188@gmail.com>
2025-07-25 00:46:18 +0300

    удалены все старые модули, добавлен файл с описанием иерархии проекта

--- old_modules/set_config.py
+++
@@ -1,8 +0,0 @@
-import configparser
-
-_parser = configparser.ConfigParser()
-_parser.read(".configs.ini")
-
-
-TOKEN: str = _parser["TOKENS"]["token"]
-SANDBOX_TOKEN: str = _parser["TOKENS"]["sandbox"]
