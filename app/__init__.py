5d0049935f420d0d8d80cf7b44e9bc10667377e6
Pavel <asinskijp188@gmail.com>
2025-08-05 22:30:57 +0300

    исправлена ошибка импорта модуля, которого нет в Unix-системах (модуль msvcrt в классе <ConsoleMenu>)

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,16 @@
+"""
+Управление через консоль пока доступно только для Windows.
+На unix-системах программа может быть запущена только в режиме start_app
+"""
+
+import platform
+PLATFORM = platform.system()
+
 from .logger import Logger
 from .asset_templates import CandleTemplate
 from .asset_templates import Strategy, TrendFollowing
 from .control_hub import ControlHub
 from .csv_saver import CSV_Saver
-from .console import Console
+
+if PLATFORM == "Windows":
+    from .console import Console
