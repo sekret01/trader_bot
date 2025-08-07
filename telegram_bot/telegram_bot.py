5c16568dae029dbbad1a32229e8c5753ef6d9a6a
Pavel <asinskijp188@gmail.com>
2025-08-07 21:52:53 +0300

    дополнение каркаса модуля telegram_bot.py необходимыми функциями, изменение в файле конфигураций start_app.ini, которые возможно будут нужны для работы сервиса

--- telegram_bot/telegram_bot.py
+++ telegram_bot/telegram_bot.py
@@ -1,29 +1,69 @@
 """
 Телеграм бот для управления работой тредер-бота.
 
-version: 0.0 
+version: 0.1
 """
 
 import telebot
 from telebot import types
+import configparser
 
 
 def get_token() -> str:
     """ Получение токена """
-    return "..."
+    parser = configparser.ConfigParser()
+    parser.read("../configs/.configs.ini")  # без ../ в итоговом варианте
+    return parser["TOKENS"]["telegram"]
 
 
-bot = telebot.TeleBot("...")
+token = get_token()
+bot = telebot.TeleBot(token)
 
 main_menu = types.ReplyKeyboardMarkup(row_width=2)
 main_menu_btn_1 = types.KeyboardButton("включить")  # text="Статус", callback_data="status")
 main_menu_btn_2 = types.KeyboardButton("выключить")
 main_menu_btn_3 = types.KeyboardButton("отчет о балансе")
-main_menu_btn_4 = types.KeyboardButton("отчет о работе")
+main_menu_btn_4 = types.KeyboardButton("отчет об операциях")
 main_menu_btn_5 = types.KeyboardButton("получить csv-отчеты")
 main_menu_btn_6 = types.KeyboardButton("получить log-файл")
 main_menu.add(main_menu_btn_1, main_menu_btn_2, main_menu_btn_3, main_menu_btn_4, main_menu_btn_5, main_menu_btn_6)
 
+
+def get_work_status() -> int:
+    """ Получение статуса работы сервиса, авто настройка кнопки на панели """
+    pass
+
+def stop_service() -> None:
+    """ Остановка работы сервиса трейдинга """
+    pass
+
+def start_service() -> None:
+    """ Запуск сервиса трейдинга """
+    pass
+
+def get_market_report() -> ...:
+    """ Создание отчета об операциях бота за день """
+    pass
+
+def get_balance_report() -> ...:
+    """ Создание отчета о балансе счета и распределении активов на данный момент """
+    pass
+
+def load_csv_reports() -> ...:
+    """ Загрузка csv-данных, хранящихся на сервере (файлы balance_data.csv и market_data.csv) """
+    pass
+
+def load_logs() -> ...:
+    """ Загрузка log-файла, хранящегося на сервере """
+    pass
+
+
+
+
+"""
+main_menu.keyboard[0][0]['text'] = "qweqweqwe"
+print(main_menu.keyboard[0][0])
+"""
 
 # MESSAGES
 
@@ -36,20 +76,26 @@ def other_messages(message):
 
     if message.text == "выключить":
         bot.reply_to(message, "Выключение сервиса...")
+        stop_service()
     elif message.text == "включить":
         bot.reply_to(message, "Включение сервиса...")
+        start_service()
 
     elif message.text == "отчет о балансе":
         bot.reply_to(message, "отчет о балансе")
+        get_balance_report()
         ... 
-    elif message.text == "отчет о работе":
-        bot.reply_to(message, "отчет о работе")
+    elif message.text == "отчет об операциях":
+        bot.reply_to(message, "отчет об операциях")
+        get_market_report()
         ...
     elif message.text == "получить csv-отчеты":
         bot.reply_to(message, "получить csv-отчеты")
+        load_csv_reports()
         ...
     elif message.text == "получить log-файл":
         bot.reply_to(message, "получить log-файл")
+        load_logs()
         ...
 
     elif message.text == "...":
